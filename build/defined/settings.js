57c7207b2444c165911f9b18490fd04b9a765472
Roberto Ulloa <roberto.ur@gmail.com>
2019-09-24 17:13:27 +0200

    fixing settings.js

--- build/defined/settings.js
+++ build/defined/settings.js
@@ -3,14 +3,14 @@ var settings = {
     name: "WebTrack"
   },
   //id: 'de.unild.webtrack',
-  //id: 'localhost',
+  id: 'localhost',
   lang: 'de',
   versionType: 'Chrome',
   mobile: false,
   requireVersion: {
     chrome: 45
   },
-  server: "https://webtrack.uni-landau.de:8443/"
+  //server: "https://webtrack.uni-landau.de:8443/"
   server: "https://localhost:8443/"
   // server: "https://va.uni-landau.de:8443/"
 }
