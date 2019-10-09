456cb79a1eec4f4b43ffc900daca2dca50d75017
Roberto Ulloa <roberto.ur@gmail.com>
2019-10-09 11:36:06 +0200

    settings for the bern server

--- build/defined/settings.js
+++ build/defined/settings.js
@@ -2,15 +2,12 @@ var settings = {
   companie: {
     name: "WebTrack"
   },
-  //id: 'de.unild.webtrack',
-  id: 'localhost',
+  id: 'webtracker.sowi.unibe.ch',
   lang: 'de',
   versionType: 'Chrome',
   mobile: false,
   requireVersion: {
     chrome: 45
   },
-  //server: "https://webtrack.uni-landau.de:8443/"
-  server: "https://localhost:8443/"
-  // server: "https://va.uni-landau.de:8443/"
+  server: "https://webtracker.sowi.unibe.ch:8443/"
 }
